ba42f9d61bf7369620e5c999d5c22b5de055747e
KatherineColon <katherinecolon@onmail.com>
2024-04-17 14:21:19 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+
+public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+// workbook already gave default values and wanted constructor to be parameterless
+
+        this.serialNumber = 0;
+        this.model = " ";
+        this.carrier = " ";
+        this.phoneNumber = " ";
+        this.owner = " ";
+
+    }
+    //setter methods
+    public void setSerialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
+
+    //getter methods
+    public int getSerialNumber(){
+        return this.serialNumber;
+    }
+    public String getModel(){
+     return this.model;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public String getOwner(){
+        return this.owner;
+    }
+}
+
+
+
+
+
---
+++ src/main/java/com/pluralsight/CellPhoneApp.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class CellPhoneApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone cellPhone = new CellPhone();
+
+        System.out.print("Please enter the serial number: ");
+            int serialNumber = scanner.nextInt(); scanner.nextLine();
+                cellPhone.setSerialNumber(serialNumber);
+        System.out.print("Please enter the model: ");
+            String model = scanner.nextLine();
+                cellPhone.setModel(model);
+        System.out.print("Please enter the carrier: ");
+            String carrier = scanner.nextLine();
+                cellPhone.setCarrier(carrier);
+        System.out.print("Please enter the phone number: ");
+            String phoneNumber = scanner.nextLine();
+                cellPhone.setPhoneNumber(phoneNumber);
+        System.out.print("Please enter the owner name: ");
+            String owner = scanner.nextLine();
+                cellPhone.setOwner(owner);
+
+
+        System.out.println("Serial number: " + cellPhone.getSerialNumber());
+        System.out.println("Model: " + cellPhone.getModel());
+        System.out.println("Carrier: " + cellPhone.getCarrier());
+        System.out.println("Phone number: " + cellPhone.getPhoneNumber());
+        System.out.println("Owner: " + cellPhone.getOwner());
+    }
+}
